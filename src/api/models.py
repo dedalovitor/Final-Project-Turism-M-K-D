204233c23b3c87734e5aab001bc7293871aa133e
KenjiRage <68568868+KenjiRage@users.noreply.github.com>
2023-02-04 21:52:20 +0100

    Update models.py

--- src/api/models.py
+++ src/api/models.py
@@ -35,7 +35,6 @@ class User_region(db.Model):
 
     regions = db.relationship('Region')
 
-
     def serialize(self):
         return {
             "id": self.id,
@@ -60,12 +59,12 @@ class Region(db.Model):
     logo = db.Column(db.String(255), nullable=False)
     user_region = db.Column(db.Integer, db.ForeignKey('user_region.id'))
     
-
-
-    restaurants = db.relationship('Restaurant')
-    hostels = db.relationship('hostel')
-    experiences = db.relationship('experience')
-    patrimonies = db.relationship('patrimony')
+    comments = db.relationship('Comments', backref='region', primaryjoin="and_(Comments.content_type=='Region', Comments.content_id==Region.id)")
+    hotels = db.relationship('Hotel', backref='region')
+    restaurants = db.relationship('Restaurant' backref='region')
+    accomodation = db.relationship('Accommodation' backref='region')
+    experiences = db.relationship('Experience' backref='region')
+    patrimonies = db.relationship('Patrimony' backref='region')
 
 
     def serialize(self):
@@ -79,20 +78,15 @@ class Region(db.Model):
             # do not serialize the password, its a security breach
         }
 
-class Region(db.Model):
+class Restoration(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100),  nullable=False)
     resume = db.Column(db.String(255), unique=False, nullable=False)
     photo = db.Column(db.String(255), nullable=False)
     logo = db.Column(db.String(255), nullable=False)
-    user_region = db.Column(db.Integer, db.ForeignKey('user_region.id'))
-    
-
-
-    restaurants = db.relationship('Restaurant')
-    hostels = db.relationship('hostel')
-    experiences = db.relationship('experience')
-    patrimonies = db.relationship('patrimony')
+    type_bussiness = db.Column(db.Enum("Bar", "Restaurante", "Chiringuito"), nullable=False)
+    user_restoration = db.Column(db.Integer, db.ForeignKey('Region.id'))
+    comments = db.relationship('Comments', backref='restoration', primaryjoin="and_(Comments.content_type=='Restoration', Comments.content_id==Restoration.id)")
 
 
     def serialize(self):
@@ -101,7 +95,87 @@ class Region(db.Model):
             "name": self.name,
             "resume": self.resume,
             "photo": self.photo,
-            "logo": self.logo
+            "logo": self.logo,
+            "type_bussiness": self.type_bussiness
+            # do not serialize the password, its a security breach
+        }
+
+class Accommodation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100),  nullable=False)
+    resume = db.Column(db.String(255), unique=False, nullable=False)
+    photo = db.Column(db.String(255), nullable=False)
+    logo = db.Column(db.String(255), nullable=False)
+    type_bussiness = db.Column(db.Enum("Hotel", "Hostal", "Albergue"), nullable=False)
+    user_accomodation = db.Column(db.Integer, db.ForeignKey('Region.id'))
+    comments = db.relationship('Comments', backref='accommodation', primaryjoin="and_(Comments.content_type=='Accommodation', Comments.content_id==Accommodation.id)")
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "resume": self.resume,
+            "photo": self.photo,
+            "logo": self.logo,
+            "type_bussiness": self.type_bussiness
 
             # do not serialize the password, its a security breach
         }
+
+class Experiences(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100),  nullable=False)
+    resume = db.Column(db.String(255), unique=False, nullable=False)
+    photo = db.Column(db.String(255), nullable=False)
+    logo = db.Column(db.String(255), nullable=False)
+    user_experiences = db.Column(db.Integer, db.ForeignKey('Region.id'))
+    comments = db.relationship('Comments', backref='experiences', primaryjoin="and_(Comments.content_type=='Experiences', Comments.content_id==Experiences.id)")
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "resume": self.resume,
+            "photo": self.photo,
+            "logo": self.logo,
+            
+            # do not serialize the password, its a security breach
+            }
+
+class Patrimony(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100),  nullable=False)
+    resume = db.Column(db.String(255), unique=False, nullable=False)
+    photo = db.Column(db.String(255), nullable=False)
+    logo = db.Column(db.String(255), nullable=False)
+    user_patrimony = db.Column(db.Integer, db.ForeignKey('Region.id'))
+    comments = db.relationship('Comments', backref='patrimony', primaryjoin="and_(Comments.content_type=='Patrimony', Comments.content_id==Patrimony.id)")
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "resume": self.resume,
+            "photo": self.photo,
+            "logo": self.logo,
+            
+            # do not serialize the password, its a security breach
+            }
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, db.ForeignKey('user.id'))    
+    region = db.Column(db.Integer, db.ForeignKey('region.id'))
+    restoration = db.Column(db.Integer, db.ForeignKey('restoration.id'))
+    accommodation = db.Column(db.Integer, db.ForeignKey('accommodation.id'))
+    patrimony = db.Column(db.Integer, db.ForeignKey('patrimony.id'))
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_region_id = db.Column(db.Integer, db.ForeignKey('user_region.id'))    
+    region = db.Column(db.Integer, db.ForeignKey('region.id'))
+    restoration = db.Column(db.Integer, db.ForeignKey('restoration.id'))
+    accommodation = db.Column(db.Integer, db.ForeignKey('accommodation.id'))
+    patrimony = db.Column(db.Integer, db.ForeignKey('patrimony.id'))
