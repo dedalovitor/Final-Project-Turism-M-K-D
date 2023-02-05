200d08c63171792ddfee1832349893d381b4162c
KenjiRage <68568868+KenjiRage@users.noreply.github.com>
2023-02-05 12:22:25 +0100

    Update models.py

--- src/api/models.py
+++ src/api/models.py
@@ -1,17 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, backref
+from enum import Enum
 db = SQLAlchemy()
-​
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-​
-    favorites = db.relationship('Favorites')
-    comments = db.relationship('Comments')
-​
+    favorites= db.relationship('Favorites') 
     def serialize(self):
         return {
             "id": self.id,
@@ -19,8 +16,9 @@ class User(db.Model):
             "name": self.name
             # do not serialize the password, its a security breach
         }
-​
-​
+    def __repr__(self):
+        return f'{self.name}'
+
 class User_region(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
@@ -32,9 +30,8 @@ class User_region(db.Model):
     address = db.Column(db.String(100), unique=True, nullable=False)
     country= db.Column(db.String(100), unique=True, nullable=False)
     city = db.Column(db.String(100), unique=True, nullable=False)
-​
     regions = db.relationship('Region')
-​
+
     def serialize(self):
         return {
             "id": self.id,
@@ -46,11 +43,12 @@ class User_region(db.Model):
             "address": self.address,
             "country": self.country,
             "city": self.city
-​
+
             # do not serialize the password, its a security breach
         }
-​
-​
+    def __repr__(self):
+        return f'{self.name}'
+
 class Region(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100),  nullable=False)
@@ -58,15 +56,11 @@ class Region(db.Model):
     photo = db.Column(db.String(255), nullable=False)
     logo = db.Column(db.String(255), nullable=False)
     user_region = db.Column(db.Integer, db.ForeignKey('user_region.id'))
-    
-    comments = db.relationship('Comments', backref='region', primaryjoin="and_(Comments.content_type=='Region', Comments.content_id==Region.id)")
-    hotels = db.relationship('Hotel', backref='region')
-    restaurants = db.relationship('Restaurant' backref='region')
-    accomodation = db.relationship('Accommodation' backref='region')
-    experiences = db.relationship('Experience' backref='region')
-    patrimonies = db.relationship('Patrimony' backref='region')
-​
-​
+    restorations = db.relationship('Restoration' ,backref='region')
+    accomodation = db.relationship('Accommodation', backref='region')
+    experiences = db.relationship('Experience' ,backref='region')
+    patrimonies = db.relationship('Patrimony' ,backref='region')
+
     def serialize(self):
         return {
             "id": self.id,
@@ -74,21 +68,29 @@ class Region(db.Model):
             "resume": self.resume,
             "photo": self.photo,
             "logo": self.logo
-​
+
             # do not serialize the password, its a security breach
         }
-​
+    def __repr__(self):
+        return f'{self.name}'
+
+class RestorationChoices(Enum):
+    bar= "bar"
+    chiringuito= "chiringuito"
+    restaurante= "restaurante"
+
+
 class Restoration(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100),  nullable=False)
     resume = db.Column(db.Text, unique=False, nullable=False)
     photo = db.Column(db.String(255), nullable=False)
     logo = db.Column(db.String(255), nullable=False)
-    type_bussiness = db.Column(db.Enum("Bar", "Restaurante", "Chiringuito"), nullable=False)
-    user_restoration = db.Column(db.Integer, db.ForeignKey('Region.id'))
-    comments = db.relationship('Comments', backref='restoration', primaryjoin="and_(Comments.content_type=='Restoration', Comments.content_id==Restoration.id)")
-​
-​
+    type_bussiness = db.Column(db.Enum(RestorationChoices), nullable=False, server_default="bar")
+    region_id = db.Column(db.Integer, db.ForeignKey('region.id'))
+    
+
+
     def serialize(self):
         return {
             "id": self.id,
@@ -99,18 +101,24 @@ class Restoration(db.Model):
             "type_bussiness": self.type_bussiness
             # do not serialize the password, its a security breach
         }
-​
+
+    def __repr__(self):
+        return f'{self.name}'
+class AccommodationChoices(Enum):
+    hotel= "hotel"
+    hostal= "hostal"
+    albergue= "albergue"
+
 class Accommodation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100),  nullable=False)
     resume = db.Column(db.Text, unique=False, nullable=False)
     photo = db.Column(db.String(255), nullable=False)
     logo = db.Column(db.String(255), nullable=False)
-    type_bussiness = db.Column(db.Enum("Hotel", "Hostal", "Albergue"), nullable=False)
-    user_accomodation = db.Column(db.Integer, db.ForeignKey('Region.id'))
-    comments = db.relationship('Comments', backref='accommodation', primaryjoin="and_(Comments.content_type=='Accommodation', Comments.content_id==Accommodation.id)")
-​
-​
+    type_bussiness = db.Column(db.Enum(AccommodationChoices), nullable=False, server_default="hotel")
+    region_id = db.Column(db.Integer, db.ForeignKey('region.id'))
+    
+
     def serialize(self):
         return {
             "id": self.id,
@@ -119,20 +127,21 @@ class Accommodation(db.Model):
             "photo": self.photo,
             "logo": self.logo,
             "type_bussiness": self.type_bussiness
-​
+
             # do not serialize the password, its a security breach
         }
-​
-class Experiences(db.Model):
+    def __repr__(self):
+        return f'{self.name}'
+class Experience(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100),  nullable=False)
     resume = db.Column(db.Text, unique=False, nullable=False)
     photo = db.Column(db.String(255), nullable=False)
     logo = db.Column(db.String(255), nullable=False)
-    user_experiences = db.Column(db.Integer, db.ForeignKey('Region.id'))
-    comments = db.relationship('Comments', backref='experiences', primaryjoin="and_(Comments.content_type=='Experiences', Comments.content_id==Experiences.id)")
-​
-​
+    region_id = db.Column(db.Integer, db.ForeignKey('region.id'))
+    
+
+
     def serialize(self):
         return {
             "id": self.id,
@@ -143,17 +152,19 @@ class Experiences(db.Model):
             
             # do not serialize the password, its a security breach
             }
-​
+
+    def __repr__(self):
+        return f'{self.name}'
+
 class Patrimony(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100),  nullable=False)
     resume = db.Column(db.Text, unique=False, nullable=False)
     photo = db.Column(db.String(255), nullable=False)
     logo = db.Column(db.String(255), nullable=False)
-    user_patrimony = db.Column(db.Integer, db.ForeignKey('Region.id'))
-    comments = db.relationship('Comments', backref='patrimony', primaryjoin="and_(Comments.content_type=='Patrimony', Comments.content_id==Patrimony.id)")
-​
-​
+    region_id = db.Column(db.Integer, db.ForeignKey('region.id'))
+    
+
     def serialize(self):
         return {
             "id": self.id,
@@ -164,19 +175,34 @@ class Patrimony(db.Model):
             
             # do not serialize the password, its a security breach
             }
-​
+    def __repr__(self):
+        return f'{self.name}'
 class Favorites(db.Model):
-    id = db.Column(db.Integer, db.ForeignKey('user.id'))    
-    region = db.Column(db.Integer, db.ForeignKey('region.id'))
-    restoration = db.Column(db.Integer, db.ForeignKey('restoration.id'))
-    accommodation = db.Column(db.Integer, db.ForeignKey('accommodation.id'))
-    patrimony = db.Column(db.Integer, db.ForeignKey('patrimony.id'))
-​
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) 
+    user= db.relationship('User')   
+    region_id = db.Column(db.Integer, db.ForeignKey('region.id'))
+    region = db.relationship('Region')
+    restoration_id = db.Column(db.Integer, db.ForeignKey('restoration.id'))
+    restoration = db.relationship('Restoration')
+    accommodation_id = db.Column(db.Integer, db.ForeignKey('accommodation.id'))
+    accommodation = db.relationship('Accommodation')
+    patrimony_id = db.Column(db.Integer, db.ForeignKey('patrimony.id'))
+    patrimony = db.relationship('Patrimony')
+
 class Comments(db.Model):
-    id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user= db.relationship('User')
+    user_region= db.relationship('User_region')
+    user_region_id =db.Column(db.Integer, db.ForeignKey('user_region.id'))
     text = db.Column(db.Text, nullable=False)
-    user_region_id = db.Column(db.Integer, db.ForeignKey('user_region.id'))    
-    region = db.Column(db.Integer, db.ForeignKey('region.id'))
-    restoration = db.Column(db.Integer, db.ForeignKey('restoration.id'))
-    accommodation = db.Column(db.Integer, db.ForeignKey('accommodation.id'))
-    patrimony = db.Column(db.Integer, db.ForeignKey('patrimony.id'))
+    region_id = db.Column(db.Integer, db.ForeignKey('region.id'))
+    region = db.relationship('Region')
+    restoration_id = db.Column(db.Integer, db.ForeignKey('restoration.id'))
+    restoration = db.relationship('Restoration')
+    accommodation_id = db.Column(db.Integer, db.ForeignKey('accommodation.id'))
+    accommodation = db.relationship('Accommodation')
+    patrimony_id = db.Column(db.Integer, db.ForeignKey('patrimony.id'))
+    patrimony = db.relationship('Patrimony')
+
